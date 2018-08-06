c6a282ceff2875e89207a5685528769c95ee9f99
Bert.Braun <bert.braun@feig.de>
2018-08-06 14:01:41 +0200

    Insertion of MCC based country name

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AT_DBConv
-Convert MD380 user.bin into AT-D868 Digital Contacts import.csv
+
+Converts MD380 user.bin into AT-D868 Digital Contacts import.csv
+
+- For inserting the respective country name, grab MCC csv list from
+  https://clients.txtnation.com/hc/en-us/article_attachments/206752938/MCCMNCs_v2.csv
+  Maintain the filename MCCMNCs_v2.csv
+
--- main.py
+++ main.py
@@ -7,15 +7,14 @@ Created on 26.07.2018
 
 import argparse
 
-#INFILE = 'user.bin'
-#OUTFILE = 'import.csv'
 
 # predefined fields
 STATEDICTGER = {0:'SA/MVP', 1:"BE", 2:"HH/SH", 3:"NS", 4:"NRW", 5:"RP/SR", 6:"HE", 7:"BW", 8:"BY", 9:"SN/TH"}
-COUNTRY = 'DMR'
+COUNTRY = 'DMR' # This could be generated automatically from ID if we had a dictionary MCC -> country name ;-)
 CALLTYPE = 'Private Call'
 CALLALERT = 'None'
 OUTFORMAT = '{0},{1},{2},{3},{4},{5},{6},{7},{8}\n'
+MCCCSVFILE='MCCMNCs_v2.csv'
 
 '''
 Format user.bin:
@@ -29,11 +28,37 @@ Format OUTFILE:
 ID, Callsign, Name, City, State, Country, Remarks, CallType, CallAlert
 '''
 
+def build_mcc_dict (CSVFILE):
+    print ('Building MCC database... ', end='')
+    mccdict={}
+    with open (CSVFILE, 'r', encoding='ISO8859-15') as dfile:
+        cnt=0
+        for line in dfile:
+            line = line.strip()
+            (mccmnc, mcc, mnc, operator) = line.split(',,', 4)
+            country=operator.split(',')[0]
+            if mcc in mccdict or mcc == 'MCC':
+                continue
+            else:
+                mccdict[mcc]=country
+                cnt += 1
+            
+    print ('{} MCCs found.'.format (cnt))
+
+    # add static entries not listed in MCC file
+    mccdict['263'] = 'Germany' 
+    mccdict['264'] = 'Germany' 
+    mccdict['314'] = 'United States'
+    mccdict['315'] = 'United States'
+    return mccdict
+
 
 parser = argparse.ArgumentParser(description = 'Convert MD380 user.bin into AT-D868 Digital Contacts import file')
 parser.add_argument('infile', metavar='Infile', type=str, help='Input file (user.bin)')
 parser.add_argument('outfile', metavar='Outfile', type=str, help='Output file (*.csv)')
 args=parser.parse_args()
+
+mccdict=build_mcc_dict(MCCCSVFILE)
 
 with open (args.infile, 'r', encoding='iso8859-15') as filein, open (args.outfile, 'w', encoding='iso8859-15') as fileout:
     cnt = 0
@@ -41,21 +66,25 @@ with open (args.infile, 'r', encoding='iso8859-15') as filein, open (args.outfil
     for line in filein:
         print (cnt+1, end='\r')
         line = line.strip() # discard leading spaces and trailing NLs
-        if len(line) == 7 or ',Time,' in line: # erste und letzte Zeile in user.bin verwerfen
+        if len(line) == 7 or ',Time,' in line: # discard both 1st and last line of user.bin
             continue
-        (dmrid, call, pname, city, state, remark, dummy) = line.split(',', 6)
+        (dmrid, call, pname, city, state, remark, dummy) = line.split(',', 6) # extract fields
         if dmrid == call or len(dmrid) == 6: # drop reflector entries as well as any repeater IDs
-            if dmrid == '262997': # keep BM parrot entry
+            if dmrid == '262997': # but keep BM parrot entry
                 pass
             else:
                 continue
         cnt += 1
         mcc = dmrid[0:3]
-        if mcc == '262' or mcc == '263': # different formatting for german calls
-            gst = int(dmrid[3]) # extract german state id
-            lineout = OUTFORMAT.format(dmrid, call, pname, state, STATEDICTGER[gst], COUNTRY, remark, CALLTYPE, CALLALERT)
+        if mcc in mccdict: # look up for country name based on MCC
+            cntry = mccdict[mcc]
         else:
-            lineout = OUTFORMAT.format(dmrid, call, pname, city, state, COUNTRY, remark, CALLTYPE, CALLALERT) 
+            cntry = COUNTRY
+        if mcc == '262' or mcc == '263': # different processing for german calls, see initial comment box
+            gst = int(dmrid[3]) # extract german state id
+            lineout = OUTFORMAT.format(dmrid, call, pname, state, STATEDICTGER[gst], cntry, remark, CALLTYPE, CALLALERT)
+        else:
+            lineout = OUTFORMAT.format(dmrid, call, pname, city, state, cntry, remark, CALLTYPE, CALLALERT) 
         fileout.write(lineout)
 
 print ("\nAll done!\n")
